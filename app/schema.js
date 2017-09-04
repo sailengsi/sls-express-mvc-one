7579cfe44e47246fc4f516554b6a7ab67c72db38
sailengsi <sailengsi@126.com>
2017-09-04 20:57:02 +0800

    schema配置成功，待测试在调用接口的时候自动执行schema？

--- app/schema.js
+++ app/schema.js
@@ -4,6 +4,15 @@ if(model){
 			return model.schema.createTable('demo_test',t=>{
 				console.log(t);
 				t.increments('id').primary();
+				t.string('title');
+				t.text('content','longtext');
+			});
+		}else{
+			return model.schema.table('demo_test',t=>{
+				console.log(t);
+				// t.increments('id').primary();
+				t.string('title');
+				t.text('content','longtext');
 			});
 		}
 	});
